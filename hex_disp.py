885cd3df1944d57eab1109e916b10f52ea7aed95
LXQ <lmyykxy@163.com>
2023-03-25 10:38:21 +0800

    adjusting bit endian

--- hex_disp.py
+++ hex_disp.py
@@ -6,11 +6,11 @@ def hex_disp(hex_string,reverse=1):
         re_bin_string = reverse_binary(binary_string)
     else:
         re_bin_string = binary_string
-    print("\nByte:0")
+    print("\nByte:%d" % ((len(re_bin_string)+1)/8 - 1))
     for i in range(len(re_bin_string)):
-        print("\t[",i,"\t]:",re_bin_string[i], end='')
-        if((i+1)%8==0):
-            print("\nByte:%d" % ((i+1)/8))
+        print("\t[",len(re_bin_string) - i - 1,"\t]:",re_bin_string[i], end='')
+        if((i+1)%8==0 and i != len(re_bin_string) - 1):
+            print("\nByte:%d" % ((len(re_bin_string)+1)/8 - (i+1)/8 - 1))
         
 
     
